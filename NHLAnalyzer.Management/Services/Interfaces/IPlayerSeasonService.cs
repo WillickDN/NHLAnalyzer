8abdfaedb4da2dea5e137af6278e0e9f11a9e7cd
WillickDN <dylan.willick.92@gmail.com>
2023-04-20 06:48:51 -0600

    Add placeholder for PlayerSeason Page

---
+++ NHLAnalyzer.Management/Services/Interfaces/IPlayerSeasonService.cs
@@ -0,0 +1,9 @@
+﻿using NHLAnalyzer.Data.Entities;
+
+namespace NHLAnalyzer.Management.Services.Interfaces
+{
+    public interface IPlayerSeasonService
+    {
+        Task<List<PlayerSeason>> GetPlayerSeasonsByYearAsync(int season);
+    }
+}
---
+++ NHLAnalyzer.Management/Services/PlayerSeasonService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NHLAnalyzer.Data;
+using NHLAnalyzer.Data.Entities;
+using NHLAnalyzer.Management.Services.Interfaces;
+
+namespace NHLAnalyzer.Management.Services
+{
+    public class PlayerSeasonService : IPlayerSeasonService
+    {
+        #region Member Variables
+
+        private readonly ApplicationDbContext _context;
+
+        #endregion
+
+        #region Constructors
+
+        public PlayerSeasonService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<List<PlayerSeason>> GetPlayerSeasonsByYearAsync(int season)
+        {
+            return await _context.PlayerSeasons.Include(x => x.Season).Include(x => x.Player).Where(x => x.Season.SeasonYear == season).ToListAsync();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
